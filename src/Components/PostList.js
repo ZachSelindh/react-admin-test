0b22b0ee1cd2d6dc4363aac71ecfc38558345ddb
Zach Selindh <zachselindh@gmail.com>
2021-01-28 13:22:34 -0600

    Changed referencefield for posts to show user's name instead of ID.

--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -4,8 +4,9 @@ import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
 export const PostList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
+            {/* ReferenceField simply fetchs refernce data as passes it as record to its children */}
             <ReferenceField source="userId" reference="users">
-                <TextField source="id" />
+                <TextField source="name" />
             </ReferenceField>
             <TextField source="id" />
             <TextField source="title" />
